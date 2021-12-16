90831fdccc7b8dd144642e51bfed6750d66dbd5e
Axe-hyx <hyx2cad@zju.edu.cn>
2021-12-16 11:59:15 +0800

    MRE float calculate error

--- unit.py
+++ unit.py
@@ -6,18 +6,14 @@ import math
 
 ti.init(arch = ti.cpu)
 
-# number of lines
-N_Line = 12
-# line space distance
-dlx = 0.009
 # type2 particle count per line
 ln_type2 = 200
 
 start_pos = ti.Vector([0.2, 0.8])
 
 ln_type3 = ln_type2 - 1
-n_type2 = N_Line* ln_type2
-n_type3 = N_Line* ln_type3
+n_type2 =  ln_type2
+n_type3 =  ln_type3
 
 #line length
 Length = 0.75
@@ -29,8 +25,6 @@ x2 = ti.Vector.field(2, dtype=float, shape=n_type2) # position
 #type3
 D3_inv = ti.Matrix.field(2, 2, dtype=float, shape=n_type3)
 d3 = ti.Matrix.field(2, 2, dtype=float, shape=n_type3)
-
-n_segment = n_type3
 
 ROT90 = ti.Matrix([[0,-1.0],[1.0,0]])
 
@@ -44,9 +38,9 @@ def GetType2FromType3(index):
 def initialize():
     for i in range(n_type2):
         sq = i // ln_type2
-        x2[i] = ti.Vector([start_pos[0]+ (i- sq* ln_type2) * sl, start_pos[1] + sq* dlx])
+        x2[i] = ti.Vector([start_pos[0]+ (i- sq* ln_type2) * sl, start_pos[1]])
 
-    for i in range(n_segment):
+    for i in range(n_type3):
         l, n = GetType2FromType3(i)
 
         # error here, dp0 is not guaranteed the same for every point
@@ -66,6 +60,7 @@ def main():
     for i in range(n_type3):
         f.write(str(i) + "\nD3_inv\n" + str(D3_inv_ny[i]) + "\nd3\n" + str(d3_ny[i]) + "\n")
 
+    print("see results/iii" + str(frame) + " veretex 3 is error")
     f.close()
 
 if __name__ == "__main__":
